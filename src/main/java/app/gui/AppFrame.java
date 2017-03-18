0916fe937e3e28eafe1db766a2ed7bd75035a68b
Sean Francis N. Ballais <sean@seanballais.com>
2017-03-19 00:32:07 +0800

    Finished adding Huffman Coding dialog.

--- src/main/java/app/gui/AppFrame.java
+++ src/main/java/app/gui/AppFrame.java
@@ -5,17 +5,22 @@ import app.utils.exceptions.FileFormatException;
 import app.utils.exceptions.NoSuchCharacterInMappingException;
 
 import javax.swing.JFrame;
-import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JRadioButton;
 import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
 import javax.swing.event.DocumentListener;
@@ -25,6 +30,8 @@ import java.awt.event.ActionListener;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AppFrame extends JFrame implements ActionListener
 {
@@ -41,7 +48,7 @@ public class AppFrame extends JFrame implements ActionListener
 	private JLabel modeMessageLabel;
 	private JTextField targetTextField;
 	private JTextField outputTextField;
-	private JScrollBar outputTextScrollBar;
+	private JScrollPane outputTextScrollPane;
 	private JRadioButton inputModeRButton;
 	private JRadioButton fileModeRButton;
 	private ButtonGroup modeButtonGroup;
@@ -52,6 +59,7 @@ public class AppFrame extends JFrame implements ActionListener
 	private JButton resetButton;
 	
 	private HuffmanTree tree;
+	private JTable huffmanCodingTable;
 	
 	public AppFrame()
 	{
@@ -72,9 +80,14 @@ public class AppFrame extends JFrame implements ActionListener
 			this.compressedText(this.targetTextField.getText());
 		} else if (event.getSource() == this.decompressButton) {
 			this.decompressText(this.targetTextField.getText());
+		} else if (event.getSource() == this.showCodingButton) {
+			this.showCoding();
 		} else if (event.getSource() == this.resetButton) {
 			this.tree.cleanup();
 			this.resetButton.setEnabled(false);
+			this.showCodingButton.setEnabled(false);
+			
+			this.huffmanCodingTable = null;
 		}
 	}
 	
@@ -85,7 +98,7 @@ public class AppFrame extends JFrame implements ActionListener
 			try {
 				compressedText = tree.compressFromString(targetText);
 			} catch (NoSuchCharacterInMappingException nex) {
-				JOptionPane.showMessageDialog(null, "Your input string contains characters that do not have a corresponding Huffman code. We suggest regenerating the Huffman tree and coding by clicking 'Reset Tree'.", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "<html>Your input string contains characters that do not have a corresponding Huffman code. We suggest regenerating<br>the Huffman tree and coding by clicking 'Reset Tree' and then 'Compress'.</html>", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
 				return;
 			}
 		} else if (this.fileModeRButton.isSelected()) {
@@ -106,19 +119,19 @@ public class AppFrame extends JFrame implements ActionListener
 					compressedText = tree.compressFromFile(targetText, "");	
 				}
 			} catch (IOException iex) {
-				JOptionPane.showMessageDialog(null, "Oops! We had a problem retrieving the selected file. Make sure it exists and you have permissions to access the file.", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "<html>Oops! We had a problem retrieving the selected file. Make sure it<br>exists and you have permissions to access the file.", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
 				return;
 			} catch (FileFormatException ffex) {
-				JOptionPane.showMessageDialog(null, ffex.getMessage() + ". Make sure each line in the file follows the format: <character (there must only be one here)>-<frequency (must be an integer)>.", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "<html>" + ffex.getMessage() + "Make sure each line in the file follows the format:<br><character (there must only be one here)>-<frequency (must be an integer)>.</html>", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
 				return;
 			} catch (NoSuchCharacterInMappingException nex) {
-				JOptionPane.showMessageDialog(null, "Your input string contains characters that do not have a corresponding Huffman code. We suggest regenerating the Huffman tree and coding by clicking 'Reset Tree'.", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
+				JOptionPane.showMessageDialog(null, "<html>Your input string contains characters that do not have a corresponding Huffman code<br>from the given file. We suggest adding the anomalous character to your statistical distribution file.</html>", "An error while compressing text", JOptionPane.ERROR_MESSAGE);
 				return;
 			}
 		}
 		
 		this.resetButton.setEnabled(true);
-		this.outputTextScrollBar.setEnabled(true);
+		this.showCodingButton.setEnabled(true);
 		this.outputTextField.setCaretPosition(0);		
 		this.outputTextField.setText(compressedText);
 	}
@@ -126,6 +139,101 @@ public class AppFrame extends JFrame implements ActionListener
 	private void decompressText(String compressedText)
 	{
 		this.outputTextField.setText(this.tree.decompress(compressedText));
+	}
+	
+	private void showCoding()
+	{
+		if (this.huffmanCodingTable == null) {
+			String[] columnNames = {
+				"Character",
+				"Huffman Coding"
+			};
+			DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0) {
+				private static final long serialVersionUID = -8842243359392592203L;
+
+				@Override public boolean isCellEditable(int row, int column) {
+					return false;
+				}
+			};
+						
+			this.populateTable(tableModel);
+			tableModel.removeRow(0);;
+			
+			this.huffmanCodingTable = new JTable(tableModel);			
+		}
+		
+		this.displayCodingDialog(this.huffmanCodingTable);
+	}
+	
+	private void populateTable(DefaultTableModel tableModel)
+	{
+		HashMap<Character, String> coding = this.tree.getCoding();
+		for (Map.Entry<Character, String> entry : coding.entrySet()) {
+			String characterColumnData = "";
+			char key = entry.getKey();
+			
+			/*
+			 *  Add these character checks in case the user adds a character
+			 *  that many any of the cases below.
+			 */
+			if (key == ' ') {
+				characterColumnData = "(space)";
+			} else if (key == '\t') {
+				characterColumnData = "(horizontal tabulation)";
+			} else if (key == '\u00A0') {
+				characterColumnData = "(space separator)";
+			} else if (key == '\u2007') {
+				characterColumnData = "(line separator)";
+			} else if (key == '\u202F') {
+				characterColumnData = "(paragraph separator)";
+			} else if (key == '\n') {
+				characterColumnData = "(new line)";
+			} else if (key == '\u000B') {
+				characterColumnData = "(vertical tabulation)";
+			} else if (key == '\f') {
+				characterColumnData = "(form feed)";
+			} else if (key == '\r') {
+				characterColumnData = "(carriage return)";
+			} else if (key == '\u001C') {
+				characterColumnData = "(file separator)";
+			} else if (key == '\u001D') {
+				characterColumnData = "(group separator)";
+			} else if (key == '\u001E') {
+				characterColumnData = "(record separator)";
+			} else if (key == '\u001F') {
+				characterColumnData = "(unit separator)";
+			} else {
+				characterColumnData = String.valueOf(key);
+			}
+			
+			Object[] rowData = {
+				characterColumnData,
+				entry.getValue()
+			};
+			tableModel.addRow(rowData);
+		}
+	}
+	
+	private void displayCodingDialog(JTable huffmanTable)
+	{
+		class HuffmanCodeDialog extends JDialog
+		{
+			private static final long serialVersionUID = -2462417535194452642L;
+
+			public HuffmanCodeDialog(JTable huffmanTable)
+			{
+				this.setLocationRelativeTo(null);
+				this.setLayout(new BorderLayout());
+				this.add(new JScrollPane(huffmanTable, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+				this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+				this.setModalityType(ModalityType.APPLICATION_MODAL);
+				this.pack();
+				this.setTitle("Huffman Coding");
+			}
+		}
+		
+		HuffmanCodeDialog huffmanCodeDialog = new HuffmanCodeDialog(huffmanTable);
+		huffmanCodeDialog.setVisible(true);
 	}
 	
 	private void setUpGUI()
@@ -159,7 +267,7 @@ public class AppFrame extends JFrame implements ActionListener
 		this.modeMessageLabel = new JLabel(" ");
 		this.targetTextField = new JTextField();
 		this.outputTextField = new JTextField();
-		this.outputTextScrollBar = new JScrollBar(JScrollBar.HORIZONTAL);
+		this.outputTextScrollPane = new JScrollPane(this.outputTextField, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		this.inputModeRButton = new JRadioButton("Input String");
 		this.fileModeRButton = new JRadioButton("Statistical Distribution File");
 		this.modeButtonGroup = new ButtonGroup();
@@ -178,9 +286,6 @@ public class AppFrame extends JFrame implements ActionListener
 		this.showTreeButton.setEnabled(false);
 		this.showCodingButton.setEnabled(false);
 		this.resetButton.setEnabled(false);
-		this.outputTextScrollBar.setEnabled(false);
-		
-		this.outputTextScrollBar.setModel(this.outputTextField.getHorizontalVisibility());
 	}
 	
 	private void addComponentsToFrame()
@@ -211,7 +316,7 @@ public class AppFrame extends JFrame implements ActionListener
 		this.add(this.modeMessagePanel);
 		this.add(this.outputTextPanel);
 		this.add(this.outputTextField);
-		this.add(outputTextScrollBar);
+		this.add(this.outputTextScrollPane);
 	}
 	
 	private void setUpListeners()
@@ -221,6 +326,7 @@ public class AppFrame extends JFrame implements ActionListener
 		this.compressButton.addActionListener(this);
 		this.decompressButton.addActionListener(this);
 		this.resetButton.addActionListener(this);
+		this.showCodingButton.addActionListener(this);
 		
 		this.targetTextField.getDocument().addDocumentListener(new DocumentListener() {
 			public void changedUpdate(DocumentEvent e) {
